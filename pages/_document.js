28cbcad651fa536bbffd767c15a00f33eba1d6a7
Guts999 <aitboteomar@gmail.com>
2023-12-24 02:51:56 +0100

    adding adsense

--- pages/_document.js
+++ pages/_document.js
@@ -34,32 +34,21 @@ class MyDocument extends Document {
           {/* Your additional text content */}
           <div>
             <h1>YouTube Thumbnail Downloader</h1>
-            <p>How to Download YouTube Thumbnail in a Few Seconds</p>
+            <p><strong>How to Download YouTube Thumbnail in a Few Seconds</strong></p>
             <p>
-              A free tool for instantly grabbing and downloading any YouTube thumbnail.
-            </p>
-            <p>
-              Add YouTube Video URL
-            </p>
-            <p>
+              <strong>Add YouTube Video URL</strong><br />
               Just add the YouTube video URL in the corresponding field.
             </p>
             <p>
-              Auto-Generate
-            </p>
-            <p>
+              <strong>Auto-Generate</strong><br />
               The YouTube video thumbnail will be generated automatically as soon as you add the URL.
             </p>
             <p>
-              Download
-            </p>
-            <p>
+              <strong>Download</strong><br />
               Hit the Download button to store the YouTube thumbnail image on your device in .JPG format.
             </p>
             <p>
-              About YouTube Thumbnail Downloader
-            </p>
-            <p>
+              <strong>About YouTube Thumbnail Downloader</strong><br />
               The video thumbnail that’s attached to the video on such platforms as YouTube is the face of the video and can play an important role in a user's decision to click on it. Very often you might need the thumbnail of a video separately for a variety of uses: to make a collage of different video thumbnails for your blog post, design a cover image for your channel using various video thumbnails, use it in a social media post in case it’s not generated automatically, and so on.
             </p>
             <p>
