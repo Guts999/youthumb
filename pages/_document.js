45416df08ce60ab0f0429e2ec9d57cd4e3a97f5b
Guts999 <aitboteomar@gmail.com>
2024-01-01 00:56:58 +0100

    adding google tag manager

--- pages/_document.js
+++ pages/_document.js
@@ -21,12 +21,15 @@ class MyDocument extends Document {
           }} />
 
 {/* <!-- Google Tag Manager --> */}
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+<script dangerouslySetInnerHTML={{
+__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PCRVNQP3');</script>
+})(window,document,'script','dataLayer','GTM-PCRVNQP3');`,
+}} />
 {/* <!-- End Google Tag Manager --> */}
+
 
           <link
             rel="apple-touch-icon"
