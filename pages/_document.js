1bb2b562a27b1a25964c60d3966c0328b35d36e2
Guts999 <aitboteomar@gmail.com>
2023-12-31 19:28:18 +0100

    adding adsense

--- pages/_document.js
+++ pages/_document.js
@@ -26,6 +26,17 @@ class MyDocument extends Document {
           />
           <link rel="manifest" href="/favicon/site.webmanifest" />
           <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+         {/* Google Analytics script */}
+          <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-23XRVGGWSN"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-23XRVGGWSN');
+        </script>
+
           <style>
             {`
               body {
