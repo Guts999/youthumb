bd8e68a9eccec5f381e9445c5710420a9a9ba299
Guts999 <aitboteomar@gmail.com>
2024-01-08 00:24:50 +0100

    adding monetag ad code

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,8 @@
 import "../styles/index.css";
-import { Fragment, useEffect } from "react";
+import { Fragment } from "react";
 import { DefaultSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }) {
-  // Add the ad code using useEffect to ensure it runs on the client side
-  useEffect(() => {
-    // Check if initBannerAd is defined to avoid potential errors
-    if (typeof initBannerAd === 'function') {
-      initBannerAd(['7SAD156599FC152D800', 'banner']);
-    } else {
-      console.error("initBannerAd is not defined. Ad functionality might not work.");
-      // You can provide a fallback behavior or display a message to the user
-    }
-  }, []);  
-
   return (
     <Fragment>
       <DefaultSeo
@@ -27,20 +16,6 @@ function MyApp({ Component, pageProps }) {
           site_name: "Youtube Thumbnail Downloader",
         }}
       />
-
-      {/* 7searchppc ad code */}
-      <div id="7SAD156599FC152D800" data-7pub="7SAD156599FC152D800"></div>
-      <script src="https://code.adclickppc.com/7s-banner-ad.js"></script>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function() {
-              initBannerAd(['7SAD156599FC152D800', 'banner'])
-            }())
-          `,
-        }}
-      />
-
       <Component {...pageProps} />
     </Fragment>
   );
