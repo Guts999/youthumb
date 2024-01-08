a0164e1919c4f8f8e60a42182d9663ae6d6e3eba
Guts999 <aitboteomar@gmail.com>
2024-01-08 02:48:47 +0100

    adding monetag ad code

--- pages/_document.js
+++ pages/_document.js
@@ -104,13 +104,13 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             </p>
             <p>
               <strong>About YouTube Thumbnail Downloader</strong><br />
-              The video thumbnail that’s attached to the video on such platforms as YouTube is the face of the video and can play an important role in a user's decision to click on it. Very often you might need the thumbnail of a video separately for a variety of uses: to make a collage of different video thumbnails for your blog post, design a cover image for your channel using various video thumbnails, use it in a social media post in case it’s not generated automatically, and so on.
+              The video thumbnail that's attached to the video on such platforms as YouTube is the face of the video and can play an important role in a user's decision to click on it. Very often you might need the thumbnail of a video separately for a variety of uses: to make a collage of different video thumbnails for your blog post, design a cover image for your channel using various video thumbnails, use it in a social media post in case it’s not generated automatically, and so on.
             </p>
             <p>
-              In any case, it’s great to have a tool that allows quickly grabbing and downloading the thumbnail on your device. Our YouTube Thumbnail Downloader does exactly that. It allows you to generate the thumbnail of any YouTube video and download it in just a few seconds.
+              In any case, it's great to have a tool that allows quickly grabbing and downloading the thumbnail on your device. Our YouTube Thumbnail Downloader does exactly that. It allows you to generate the thumbnail of any YouTube video and download it in just a few seconds.
             </p>
             <p>
-              That’s it. You have the video thumbnail and can use it anywhere. However, some images may be subject to copyright, so make sure to get this aspect sorted before using the image publicly. You might need to ask for the author’s permission, provide an attribution, and so on. Enjoy!
+              That's it. You have the video thumbnail and can use it anywhere. However, some images may be subject to copyright, so make sure to get this aspect sorted before using the image publicly. You might need to ask for the author’s permission, provide an attribution, and so on. Enjoy!
             </p>
           </div>
         </body>
