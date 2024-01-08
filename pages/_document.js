06d2844bf240429f1ab681287cc08c99e5bb90a5
Guts999 <aitboteomar@gmail.com>
2024-01-08 02:00:47 +0100

    adding monetag ad code

--- pages/_document.js
+++ pages/_document.js
@@ -63,16 +63,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 margin: 0 auto; /* Center the div itself */
               }
             `}
-          </style>
-
-          <div id="7SAD156599FC152D800" data-7pub="7SAD156599FC152D800"></div>
-                    <script src="https://code.adclickppc.com/7s-banner-ad.js"></script>
-                    <script>
-                    (function() {
-                      initBannerAd(['7SAD156599FC152D800', 'banner'])
-                      }())
-                    </script>
-                    
+          </style>            
         </Head>
         <body>
 
